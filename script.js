2e157d0e80211ac706ed9434e123398e865dbefe
Sridhar-1312 <ronaldosriiicr7@gmail.com>
2025-07-22 23:29:30 +0530

    added share button

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 
 
+// const getquote = async () => {
+//         document.querySelector(".quote-app blockquote").innerHTML = "loading....";
+//     document.querySelector(".quote-app span").innerHTML = "loading...";
+//     const instruction = await fetch("https://dummyjson.com/quotes/random")
+//     const data = await instruction.json();
+//     // console.log(data);
+
+//     document.querySelector(".quote-app blockquote").innerHTML = data.quote;
+//     document.querySelector(".quote-app span").innerHTML = data.author;
+
+// }
+// getquote();
+
+
+let TweetData;
 const getquote = async () => {
-        document.querySelector(".quote-app blockquote").innerHTML = "loading....";
-    document.querySelector(".quote-app span").innerHTML = "loading...";
-    const instruction = await fetch("https://dummyjson.com/quotes/random")
-    const data = await instruction.json();
-    // console.log(data);
-
-    document.querySelector(".quote-app blockquote").innerHTML = data.quote;
-    document.querySelector(".quote-app span").innerHTML = data.author;
-
+    const content = await fetch("https://dummyjson.com/quotes/random").then((e) => e.json())
+    document.querySelector(".quote-app blockquote").innerHTML = content.quote;
+    document.querySelector(".quote-app span").innerHTML = content.author;
+    TweetData = content;
 }
 getquote();
+
+const share = () => {
+    window.open("https://twitter.com/intent/tweet?text=" +`${TweetData.quote}`+ "-----by  " + `${TweetData.author}`, "width=600, height=300")
+}
+
+ 
